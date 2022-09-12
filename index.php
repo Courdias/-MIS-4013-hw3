59e217a57537514a9f63b476dcbc1e9d3ee5cfa8
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-12 14:22:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,4 +24,30 @@ echo "Connected successfully";
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
+  <?php
+$servername = "localhost";
+$username = "buidemac_homework3";
+$password = "homework3user";
+$dbname = "buidemac_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT instructor_id, instructor_name, FROM Instructor";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
 </html>

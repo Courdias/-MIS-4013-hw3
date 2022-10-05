8285b0a3852f9c1474e87d662672897bbf5a573a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-04 20:03:17 -0500

    Update TableChoiceDropdown.php

--- TableChoiceDropdown.php
+++ TableChoiceDropdown.php
@@ -3,7 +3,7 @@
     Which Page?
   </a>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="table1.php">Go to Table 1</a></li>
+    <li><a class="dropdown-item" href="Tinstructor.php">Go to Instructors</a></li>
     <li><a class="dropdown-item" href="inputpageget.php">Go to Get Input Page</a></li>
   </ul>
 </div>

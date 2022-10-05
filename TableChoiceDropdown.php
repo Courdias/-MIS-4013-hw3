08a2b9634a478264f2fe0aeaabf302cf611923c4
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-04 20:23:08 -0500

    Update TableChoiceDropdown.php

--- TableChoiceDropdown.php
+++ TableChoiceDropdown.php
@@ -5,5 +5,7 @@
   <ul class="dropdown-menu">
     <li><a class="dropdown-item" href="Tinstructor.php">Go to Instructors</a></li>
     <li><a class="dropdown-item" href="Tcourse.php">Go to Courses</a></li>
+    <li><a class="dropdown-item" href="Tsection.php">Go to Sections</a></li>
+    <li><a class="dropdown-item" href="Tstudent.php">Go to Students</a></li>
   </ul>
 </div>

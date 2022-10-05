3b6cd24e51f556ba8a03c4888e0a3e9dc0443f22
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-04 19:27:50 -0500

    Update TableChoiceDropdown.php

--- TableChoiceDropdown.php
+++ TableChoiceDropdown.php
@@ -2,13 +2,12 @@
 <div class="dropdown">
   <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
   Which Page?
-  </button>
+  </a>
 
   <ul class="dropdown-menu">
     <li><a class="dropdown-item" href="table1.php">Table 1</a></li>
     <li><a class="dropdown-item" href="table2.php">Table 2</a></li>
     <li><a class="dropdown-item" href="table3.php">Table 3</a></li>
     <li><a class="dropdown-item" href="table4.php">Table 4</a></li>
-    
   </ul>
 </div>

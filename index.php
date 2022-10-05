6a9899af8b1cfec7307d123343a599c06b82b042
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-04 20:02:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <!doctype html>
 <?php
   include "header.php";
-  include "TableChoiceDropdown.php";
 ?>
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Which Page?
+  </a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="table1.php">Go to Table 1</a></li>
+    <li><a class="dropdown-item" href="inputpageget.php">Go to Get Input Page</a></li>
+  </ul>
+</div>
   <body>
 </html>
